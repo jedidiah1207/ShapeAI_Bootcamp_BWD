9e9b3f51eb2aa5171cd3d2a9ff6ef62a04da02f2
JEDIDIAH SAMUEL <77118172+jedidiah1207@users.noreply.github.com>
2021-06-26 20:55:51 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+import Info from "./Info";
+function App() {
+  return (
+    <div>
+      <Header />
+      <Footer />
+      <Info />
+      <Info />
+      <Info />
+      <Info />
+      <Info />
+    </div>
+  );
+}
+
+export default App;
---
+++ Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <footer>
+      <p>Copyright by ShapeAI A{new Date().getFullYear()}</p>
+    </footer>
+  );
+}
+
+export default Footer;
